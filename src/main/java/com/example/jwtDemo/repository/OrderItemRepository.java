803aaabe65bbf660021ac384dd903582f47de69f
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-05-16 18:28:10 +0530

    Update OrderItemRepository.java

--- src/main/java/com/example/jwtDemo/repository/OrderItemRepository.java
+++ src/main/java/com/example/jwtDemo/repository/OrderItemRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.jwtDemo.entity.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+  //new add for user order
+  List<OrderItem> findByOrderId(Long orderId);
 }
