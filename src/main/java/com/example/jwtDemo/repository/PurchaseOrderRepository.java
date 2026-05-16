23506c79861df946789ba94be3b216168b0651b1
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-05-16 18:34:02 +0530

    Update PurchaseOrderRepository.java

--- src/main/java/com/example/jwtDemo/repository/PurchaseOrderRepository.java
+++ src/main/java/com/example/jwtDemo/repository/PurchaseOrderRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.jwtDemo.entity.PurchaseOrder;
 import com.example.jwtDemo.entity.User;
 
+//new add
+import java.util.List; // 🔴 Make sure to add this import
+
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
     Optional<PurchaseOrder> findByIdAndUser(Long id, User user);
 
