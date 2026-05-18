26af303afed619c974bc6c1781d4e7b527586eb5
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-05-18 11:09:06 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/jwtDemo/config/SecurityConfig.java
+++ src/main/java/com/example/jwtDemo/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOrigin("https://cartnova-shop.vercel.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
